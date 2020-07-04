dfeb0c6db1ed1848eb11374a03d0227df8fd8998
user0 <oy8oom6r@gmail.com>
2020-07-04 17:19:53 +0300

    Revert "feed_ignore.go - cleared"
    This reverts commit 678b842a

--- core/feed.go
+++ core/feed.go
@@ -149,6 +149,8 @@ func (t *Textile) feedItem(block *pb.Block, opts feedItemOpts) (*pb.FeedItem, er
 	switch block.Type {
 	case pb.Block_MERGE:
 		payload, err = t.merge(block, opts)
+	case pb.Block_IGNORE:
+		payload, err = t.ignore(block, opts)
 	case pb.Block_FLAG:
 		payload, err = t.flag(block, opts)
 	case pb.Block_JOIN:
