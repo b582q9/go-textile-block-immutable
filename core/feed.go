678b842a76b50accaa4bab12577eebba98e6bb3a
user0 <oy8oom6r@gmail.com>
2020-07-04 16:57:41 +0300

    feed_ignore.go - cleared

--- core/feed.go
+++ core/feed.go
@@ -149,8 +149,6 @@ func (t *Textile) feedItem(block *pb.Block, opts feedItemOpts) (*pb.FeedItem, er
 	switch block.Type {
 	case pb.Block_MERGE:
 		payload, err = t.merge(block, opts)
-	case pb.Block_IGNORE:
-		payload, err = t.ignore(block, opts)
 	case pb.Block_FLAG:
 		payload, err = t.flag(block, opts)
 	case pb.Block_JOIN:
