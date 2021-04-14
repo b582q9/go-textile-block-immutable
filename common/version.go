c1bb62b21d73778e35811b193d2cb55d7208ee39
user0 <oy8oom6r@gmail.com>
2021-04-14 20:58:01 +0300

    Version changed to 0.8.0-sapien

--- common/version.go
+++ common/version.go
@@ -4,4 +4,4 @@ package common
 var GitCommit, GitBranch, GitState, GitSummary, BuildDate string
 
 // Version is the current application's version literal
-const Version = "0.7.9-sapien"
+const Version = "0.8.0-sapien"
